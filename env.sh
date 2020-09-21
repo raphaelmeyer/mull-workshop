85d19184362bfb6565f2f744219e29dab76374d8
raphael meyer <raphael.akira.meyer@gmail.com>
2020-09-21 22:56:20 +0200

    replace hardcoded path

--- env.sh
+++ env.sh
@@ -3,5 +3,5 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd ${script_dir}
-docker run --rm -it -v workspace:/workspace -v /home/raphael/Projects/mull-workshop/source:/source:ro --name mull mull-workshop
+docker run --rm -it -v workspace:/workspace -v $(pwd)/source:/source:ro --name mull mull-workshop
 
